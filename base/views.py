0ae0d5a5f37b00a6c5989432931fa093fc130dc3
Kamen <kamenkolev01@gmail.com>
2024-10-10 16:24:39 +0300

    Add a sign up view

--- base/views.py
+++ base/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from .models import UserProfile
 
 def index(request):
     return render(request, 'base/index.html')
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST, request.FILES)  # Добави request.FILES за файлове
+        if form.is_valid():
+            user = form.save(commit=False)  # Спираме временно запазването на потребителя
+            user.save()  # Запазваме потребителя
+            # Ако има качена снимка, добавяме профил със снимката
+            if form.cleaned_data.get('profile_image'):
+                profile_image = form.cleaned_data.get('profile_image')
+                UserProfile.objects.create(user=user, profile_image=profile_image)
+            else:
+                # Създаваме потребителски профил без снимка
+                UserProfile.objects.create(user=user)
+            
+            return redirect('/login/') 
+    else:
+        form = SignUpForm()
+
+    return render(request, 'sign_up.html', {'form': form})
