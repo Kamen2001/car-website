bec79c9aea086e082fe835998f4fd32fc49a5323
Kamen <kamenkolev01@gmail.com>
2024-10-10 16:00:32 +0300

    Create forms.py and add sign up form

---
+++ base/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl'
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'profile_image')
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': INPUT_CLASSES
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your password',
+        'class': INPUT_CLASSES
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Repeat password',
+        'class': INPUT_CLASSES
+    }))
+    email = forms.CharField(widget=forms.EmailField(attrs={
+        'placeholder': 'Your email address',
+        'class': INPUT_CLASSES
+    }))
+    profile_image = forms.ImageField(widget=forms.FileField(attrs={
+        'class': INPUT_CLASSES 
+    }), required=False)
+
+    
