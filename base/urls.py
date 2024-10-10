8a0768ab52bded6c55e0b4c2fec663b978dedd9e
Kamen <kamenkolev01@gmail.com>
2024-10-10 17:07:29 +0300

    Add login path

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from .forms import LoginForm
+from django.contrib import auth
 from . import views
  
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('sign/', views.sign_up, name='sign_up'),
+    path('login/', auth.views.LoginView.as_view(template_name='login.html', authentication_form=LoginForm), name='login')
 ]
