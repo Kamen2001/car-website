005c4417352f5d7722ea6bca03c4a63231822417
Kamen <kamenkolev01@gmail.com>
2024-10-11 13:38:29 +0300

    Add profile view and fix issues with edit view

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import logout
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm, EditUserProfileForm
 from .models import UserProfile
@@ -8,7 +8,7 @@ def index(request):
     return render(request, 'index.html')
 
 def sign_up(request):
-    is_edit = False
+    
     if request.method == 'POST':
         form = SignUpForm(request.POST, request.FILES)  # Добави request.FILES за файлове
         if form.is_valid():
@@ -27,8 +27,7 @@ def sign_up(request):
         form = SignUpForm()
 
     context = {
-        'form': form,  
-        'is_update': is_edit
+        'form': form
     }
 
     return render(request, 'sign_up.html', context)
@@ -40,20 +39,52 @@ def logout_view(request):
     return redirect('index') 
 
 @login_required
-def edit_profile(request):
-    is_edit = True
+def profile(request, username):
+    user = request.user  
+    user_profile = get_object_or_404(UserProfile, user=user)
+    if request.user.username != username:
+
+        return redirect('index')
+    return render(request, 'profile.html', {'user_profile': user_profile})
+
+@login_required
+def edit_profile(request, username):
+    user = request.user  
+    user_profile = get_object_or_404(UserProfile, user=user)
+
+    if request.user.username != username:
+        return redirect('index')
+
     if request.method == 'POST':
-        form = EditUserProfileForm(request.POST, request.FILES, instance=request.user)
+        form = EditUserProfileForm(request.POST, request.FILES, instance=user)
+
         if form.is_valid():
-            form.save()  
+            # Актуализиране на полетата на User
+            user.username = form.cleaned_data['username']
+            
+            # Проверка и актуализация на паролата
+            password1 = form.cleaned_data['password1']
+            password2 = form.cleaned_data['password2']
+            if password1 and password1 == password2:
+                user.set_password(password1)  # Актуализиране на паролата
+            else:
+                form.add_error('password2', 'Паролите не съвпадат.')
+
+            # Запазване на User обекта
+            user.save()
+
+            # Запазване на UserProfile
+            user_profile.profile_image = form.cleaned_data['profile_image']
+            user_profile.save()  # Запазваме промените в UserProfile
+            update_session_auth_hash(request, user) 
             return redirect('index')  
 
     else:
-        form = EditUserProfileForm(instance=request.user)  
+        form = EditUserProfileForm(instance=user)
 
     context = {
         'form': form,  
-        'is_update': is_edit
+        'username': user_profile.user.username  
     }
 
-    return render(request, 'sign_up.html', context)  
+    return render(request, 'edit_profile.html', context)
