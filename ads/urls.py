7efb3accce7d0710dc3607778166c1585df8873c
Kamen <kamenkolev01@gmail.com>
2024-10-14 16:04:02 +0300

    Create the views and map them

---
+++ ads/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('new/', views.ad_new, name='ad_new'),
+    path('my_ads/', views.my_ads, name='my_ads'),
+    path('<int:pk>/', views.ad_view, name='ad_view'),
+    path('<int:pk>/edit', views.ad_edit, name='ad_edit'),
+    path('<int:pk>/delete', views.ad_delete, name='ad_delete'),
+    path('search/', views.ad_search, name='ad_search_form'),
+    
+]
--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Ad
+from .form import NewAdForm
+from base.models import UserProfile
+from .filters import AdFilter
 
-# Create your views here.
+@login_required
+def ad_new(request):
+    if request.method == 'POST':
+        form = NewAdForm(request.POST, request.FILES)
+        if form.is_valid():
+            ad = form.save(commit=False)
+            user_profile = UserProfile.objects.get(user=request.user)
+            ad.created_by = user_profile
+            ad.save()
+            
+            return redirect('ad_view', pk=ad.id)
+    else:    
+        form = NewAdForm()
+
+    return render(request, 'ad_form.html', {'form': form})    
+
+def ad_view(request, pk):
+    ad = get_object_or_404(Ad, pk=pk)
+    
+    return render(request, 'ad_view.html', {'ad': ad})
+
+@login_required
+def ad_edit(request, pk): 
+    ad = get_object_or_404(Ad, pk=pk)
+    if request.method == 'POST':
+        form = NewAdForm(request.POST, request.FILES, instance=ad)
+        if form.is_valid():
+            ad.ad_status = True
+            form.save()
+            return redirect('ad_view', pk=ad.id)
+    else:    
+        form = NewAdForm(instance=ad)
+
+    context ={
+        'form': form,
+        'ad': ad,
+        'editing': True
+    }
+
+    return render(request, 'ad_form.html', context)
+
+@login_required
+def ad_delete(request, pk):
+    user_profile = UserProfile.objects.get(user=request.user)
+    ad = get_object_or_404(Ad, pk=pk, created_by=user_profile)
+    ad.delete()
+
+    return redirect('my_ads')
+
+@login_required
+def my_ads(request):
+    user_profile = UserProfile.objects.get(user=request.user)
+    ads = Ad.objects.filter(created_by=user_profile)
+    
+    return render(request, 'my_ads.html', {'ads': ads})
+
+def ad_search(request):
+    ad_filter = AdFilter(request.GET, queryset=Ad.objects.all()) 
+    context ={
+        'form': ad_filter.form,
+        'ad_filter': ad_filter.qs
+    }
+    return render(request, 'ad_search_form.html', context)
